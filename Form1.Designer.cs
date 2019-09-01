d07b8c1513709ef45917491534faba5f703c8e02
AlexxRA <41384229+AlexxRA@users.noreply.github.com>
2019-09-01 16:47:57 -0500

    Agregar interrupcion en proceso

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -81,6 +81,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(644, 444);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // textBox1
             // 
@@ -134,64 +135,64 @@
             // 
             // chart1
             // 
-            chartArea5.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart1.Legends.Add(legend5);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(27, 43);
             this.chart1.Name = "chart1";
-            series5.ChartArea = "ChartArea1";
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chart1.Series.Add(series5);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(162, 70);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
             // 
             // chart2
             // 
-            chartArea6.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea6);
-            legend6.Name = "Legend1";
-            this.chart2.Legends.Add(legend6);
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
             this.chart2.Location = new System.Drawing.Point(233, 43);
             this.chart2.Name = "chart2";
-            series6.ChartArea = "ChartArea1";
-            series6.Legend = "Legend1";
-            series6.Name = "Series1";
-            this.chart2.Series.Add(series6);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
             this.chart2.Size = new System.Drawing.Size(162, 70);
             this.chart2.TabIndex = 9;
             this.chart2.Text = "chart2";
             // 
             // chart3
             // 
-            chartArea7.Name = "ChartArea1";
-            this.chart3.ChartAreas.Add(chartArea7);
-            legend7.Name = "Legend1";
-            this.chart3.Legends.Add(legend7);
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
             this.chart3.Location = new System.Drawing.Point(431, 43);
             this.chart3.Name = "chart3";
-            series7.ChartArea = "ChartArea1";
-            series7.Legend = "Legend1";
-            series7.Name = "Series1";
-            this.chart3.Series.Add(series7);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
             this.chart3.Size = new System.Drawing.Size(162, 70);
             this.chart3.TabIndex = 10;
             this.chart3.Text = "chart3";
             // 
             // chart4
             // 
-            chartArea8.Name = "ChartArea1";
-            this.chart4.ChartAreas.Add(chartArea8);
-            legend8.Name = "Legend1";
-            this.chart4.Legends.Add(legend8);
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
             this.chart4.Location = new System.Drawing.Point(35, 181);
             this.chart4.Name = "chart4";
-            series8.ChartArea = "ChartArea1";
-            series8.Legend = "Legend1";
-            series8.Name = "Series1";
-            this.chart4.Series.Add(series8);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart4.Series.Add(series4);
             this.chart4.Size = new System.Drawing.Size(557, 121);
             this.chart4.TabIndex = 11;
             this.chart4.Text = "chart4";
--- Form1.cs
+++ Form1.cs
@@ -14,20 +14,27 @@ namespace AdministradorDeTareas
     public partial class Form1 : Form
     {
         ArrayList interrupciones = new ArrayList();
-        DataTable dt = new DataTable();
+        Stack enProceso = new Stack();
+
+        DataTable dataTableInterrupcion = new DataTable();
+        DataTable dataTableEnProceso = new DataTable();
 
         public Form1()
         {
-            interrupciones.Add(new Interrupcion("FF",5,20,10,5));
+            interrupciones.Add(new Interrupcion("FF", 5, 20, 10, 5));
             interrupciones.Add(new Interrupcion("1A", 4, 12, 5, 1));
             interrupciones.Add(new Interrupcion("B6", 3, 2, 3, 1));
 
-            dt.Columns.Add("numero");
-            dt.Columns.Add("nombre");
-            dt.Columns.Add("tiempo");
-            dt.Columns.Add("ram");
-            dt.Columns.Add("cpu");
-            dt.Columns.Add("hdd");
+            dataTableInterrupcion.Columns.Add("numero");
+            dataTableInterrupcion.Columns.Add("nombre");
+            dataTableInterrupcion.Columns.Add("tiempo");
+            dataTableInterrupcion.Columns.Add("ram");
+            dataTableInterrupcion.Columns.Add("cpu");
+            dataTableInterrupcion.Columns.Add("hdd");
+
+            dataTableEnProceso = dataTableInterrupcion.Copy();
+
+
 
 
             InitializeComponent();
@@ -40,20 +47,45 @@ namespace AdministradorDeTareas
             int i = 0;
             foreach (Interrupcion inter in interrupciones)
             {
-                
-                DataRow dRow = dt.NewRow();
+
+                DataRow dRow = dataTableInterrupcion.NewRow();
                 dRow["numero"] = i++;
                 dRow["nombre"] = inter.nombre;
                 dRow["tiempo"] = inter.tiempo;
                 dRow["ram"] = inter.consumoRamMegas;
                 dRow["cpu"] = inter.consumoCpu;
                 dRow["hdd"] = inter.consumoHdd;
-                dt.Rows.Add(dRow);
-                
-               
+                dataTableInterrupcion.Rows.Add(dRow);
+
+
             }
-            dataGridView1.DataSource = dt;
+            dataGridView1.DataSource = dataTableInterrupcion;
             dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string nombreInterrupcion = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+            foreach (Interrupcion inter in interrupciones)
+            {
+                if (inter.nombre.Equals(nombreInterrupcion))
+                {
+                    enProceso.Push(inter);
+
+                    DataRow dRow = dataTableEnProceso.NewRow();
+                    dRow["numero"] = 0;
+                    dRow["nombre"] = inter.nombre;
+                    dRow["tiempo"] = inter.tiempo;
+                    dRow["ram"] = inter.consumoRamMegas;
+                    dRow["cpu"] = inter.consumoCpu;
+                    dRow["hdd"] = inter.consumoHdd;
+                    dataTableEnProceso.Rows.Add(dRow);
+                }
+            }
+            dataGridView2.DataSource = dataTableEnProceso;
+            dataGridView2.Refresh();
+
         }
     }
 
